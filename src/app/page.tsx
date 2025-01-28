854935d563c7613a134772136a1a851a6f8d7066
Noufal Khan <noufal.khan@ospyn.com>
2025-01-28 15:56:45 +0530

    v4 upgrade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,31 @@ import React from 'react'
 
 const page = () => {
   return (
-    <>
-       
-      <Button>Click me</Button>
+ <div className='flex items-center justify-center h-screen flex-col  gap-5 relative'>
+      {/* <div className='text-xl font-display bg-avocado-500 hover:bg-avocado-500 transition-all ease-snappy 3xl:p-8 p-1'>12345</div> */}
+
+<div className='h-52 w-52 rounded-full bg-linear-60 from-indigo-500 via-purple-500 to-pink-500'></div>
+
+<div className="size-52 rounded-full bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% absolute top-0 left-0 animate-"></div>
+
+      </div>
+
+
     
-    </>
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
   )
 }
 
